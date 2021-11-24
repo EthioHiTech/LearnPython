3c92caf549fbc1c6cd02825f2f8317e5d320c1b7
EaglexAcademy <71806391+EaglexAcademy@users.noreply.github.com>
2021-11-23 20:38:38 -0600

    second update

--- Lesson1/inputOutputs.py
+++ Lesson1/inputOutputs.py
@@ -8,3 +8,8 @@ print("Sara")
 #print() name, prints Sara onto the screen
 name = "Sara"
 print(name) 
+
+#name is a variable to store the value that was input from the user
+#print() name, prints Sara onto the screen
+name = input("Enter your name")
+print(name)
