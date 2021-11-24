6d7a04a1ffa5f31bac62d8d3f3a0dc8db497d819
EaglexAcademy <71806391+EaglexAcademy@users.noreply.github.com>
2021-11-23 20:30:20 -0600

    First Update

---
+++ Lesson1/inputOutputs.py
@@ -0,0 +1,10 @@
+#print() function, prints a message onto the screen
+print("Hello") 
+
+#print() function, prints Sara onto the screen
+print("Sara") 
+
+#name os a variable that stores the String name value, Sara
+#print() name, prints Sara onto the screen
+name = "Sara"
+print(name) 
